27665dacd52ff7da1ae979cb45ed501b10d0be95
Angel-Lopez <manelopez.2015@gmail.com>
2024-05-22 11:38:48 -0600

    Update microdor.py

--- microdor.py
+++ microdor.py
@@ -1,13 +1,12 @@
 from microdot import Microdot
-from microdot import json_response
 app = Microdot()
 
 @app.route('/')
-async def index(request):
+async def hello_world(request):
     return 'Hello, world!'
     
 @app.route('/Auth', methods=['POST'])
-async def index(request):
-    return json_response({"R": 200, "D": 51})
+async def authenticate(request):
+    return {"R": 200, "D": 51}
 
 app.run()
