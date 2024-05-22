7b0e5dfba35f3af0abe94e0c98ea54f8e714a489
Angel-Lopez <manelopez.2015@gmail.com>
2024-05-22 11:31:11 -0600

    Update microdor.py

--- microdor.py
+++ microdor.py
@@ -5,6 +5,11 @@ app = Microdot()
 @app.route('/')
 async def index(request):
     return 'Hello, world!'
-
+    
+@app.route('/Auth', methods=['POST'])
+async def index(request):
+    # Aquí puedes realizar cualquier procesamiento necesario con los datos de la solicitud POST
+    # Por ahora, simplemente devolveremos un JSON de ejemplo
+    return json_response({"R": 200, "D": 51})
 
 app.run()
